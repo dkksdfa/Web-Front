9fb32ebb66b337447cc6e1524f3d27a6b9c990a2
wjdekdnlt <wjdekdnlt1215@gmail.com>
2020-09-12 22:57:07 +0900

    commit

--- src/pages/Join.js
+++ src/pages/Join.js
@@ -11,27 +11,6 @@ import {
   Description,
 } from "../styles/StyledJoin";
 const Join = () => {
-  const onClickHandler = (e) => {
-    // 해당 이벤트에 미리 구현되어 있는 동작이 발생하지 않도록 함
-    e.preventDefault();
-    // task의 값이 빈 문자열이 아닌 경우 실행
-    if (task !== "") {
-      firestore
-        .collection("user")
-        .add({ todo: task }) // todo 필드에 task 값 입력
-        .then((res) => {
-          // 추가된 데이터와 관련된 정보가 전달됨
-          /*
-          tasks 스테이트의 각 요소는 todo, id를 필드로 갖는 객체이기 때문에
-          todo 필드에는 task를, id에는 추가된 데이터의 id인 res.id를 대입해서
-          기존 tasks 스테이트에 추가
-          */
-          setTasks((prevTasks) => tasks.concat({ todo: task, id: res.id }));
-        });
-      // input 값을 빈 문자열로 되돌림
-      setTask("");
-    }
-  };
   return (
     <Back>
       <TitleWrap>
--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,7 +1,6 @@
 import React from "react";
 import PageWrap from "./PageWrap";
 import { Link } from "react-router-dom";
-import { GoogleAuthProvider } from "../firebase";
 import {
   Title,
   Input,
@@ -19,7 +18,6 @@ const SignIn = () => {
   // const getPassword = () => {
   //   console.log(password.current.value);
   // };
-  var provider = new GoogleAuthProvider();
   return (
     <PageWrap>
       <Title>Please sign in.</Title>
