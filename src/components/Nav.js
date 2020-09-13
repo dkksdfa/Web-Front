3e10f369f113cbb1e727e7286cebc60367296040
wjdekdnlt <wjdekdnlt1215@gmail.com>
2020-09-13 20:43:11 +0900

    edit nav link structures

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,24 +5,25 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../pages";
 const Nav = ({ main }) => {
   const [isLoggedIn, setLoggedIn] = React.useState(false);
-  const menu = [
-    { name: "School", link: "School" },
-    { name: "SW", link: "SW" },
-    { name: "Food", link: "Food" },
-    { name: "Finance", link: "Finance" },
-    { name: "Other", link: "Other" },
-  ];
   return (
     <Navigation>
       <ButtonPosition>
         <Button>
           <Link to="/">Home</Link>
         </Button>
-        {menu.map((value, index) => (
-          <Button key={index}>
-            <Link to={"/clubs/" + value.link}>{value.name}</Link>
-          </Button>
-        ))}
+        <Button>
+          <Link to="/clubs/SW">SW</Link>
+        </Button>
+        <Button>
+          <Link to="/clubs/Food">Food</Link>
+        </Button>
+        <Button>
+          <Link to="/clubs/Finance">Finance</Link>
+        </Button>
+        <Button>
+          <Link to="/clubs/Other">Other</Link>
+        </Button>
+
         <AuthContext.Provider value={{ isLoggedIn, setLoggedIn }}>
           {isLoggedIn ? (
             <>
