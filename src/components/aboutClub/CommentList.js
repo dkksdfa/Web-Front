5db400812554445c0a59b6f7ad59ff5af91adc62
dawit jeong <wjdekdnlt1215@gmail.com>
2020-11-13 11:19:04 +0900

    got some error in CommentList.js

--- src/components/aboutClub/CommentList.js
+++ src/components/aboutClub/CommentList.js
@@ -4,33 +4,50 @@ import React, { useCallback, useEffect, useState } from "react";
 const CommentList = ({ clublink }) => {
   const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([]);
+  const [names, setNames] = useState([]);
   const [error, setError] = useState(null);
   const fetchData = async () => {
     setLoading(true);
-    try {
+
+    let newNames = [];
+    const getComments = async () => {
+      const result = [];
       const dbComments = await firestore
         .collection("comments")
         .where("club", "==", clublink)
         .orderBy("date", "asc")
         .get();
-      let newComments = [];
       dbComments.forEach(async (comment) => {
-        // const dbUser = await firestore
-        //   .collection("additional userinfo")
-        //   .doc(comment.data().creatorId)
-        //   .get();
-        // const creatorName = dbUser.data().name;
-        newComments.push({ ...comment.data() /* creatorName*/ });
+        result.push(comment.data());
       });
-      setComments(newComments);
+
+      return result;
+    };
+    const getUserName = async (uid) => {
+      const dbUser = await firestore
+        .collection("additional userinfo")
+        .doc(uid)
+        .get();
+      const creatorName = dbUser.data().name;
+      return creatorName;
+    };
+    try {
+      const dbComments = getComments();
+      dbComments.forEach((val) => {
+        const creatorName = getUserName(val.creatorId);
+        newNames.push(creatorName);
+      });
+      setComments(dbComments);
     } catch (e) {
       setError(e);
     }
+    setNames(newNames);
     setLoading(false);
   };
   useEffect(() => {
     fetchData();
   }, []);
+
   return (
     <>
       {error && <div>{error}</div>}
@@ -40,8 +57,11 @@ const CommentList = ({ clublink }) => {
             <div>
               <ul>
                 {comments.map((val, i) => {
-                  console.log({ val });
-                  return <li key={i}>{val.content}</li>;
+                  return (
+                    <li key={i}>
+                      {val.content} | {names[i]}
+                    </li>
+                  );
                 })}
               </ul>
             </div>
