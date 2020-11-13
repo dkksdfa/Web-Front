8b26c814585032a722342a29372844d4a66e3234
dawit jeong <wjdekdnlt1215@gmail.com>
2020-11-13 10:31:55 +0900

    what's the problem?

--- src/components/aboutClub/CommentList.js
+++ src/components/aboutClub/CommentList.js
@@ -4,35 +4,49 @@ import React, { useCallback, useEffect, useState } from "react";
 const CommentList = ({ clublink }) => {
   const [loading, setLoading] = useState(false);
   const [comments, setComments] = useState([]);
-  const fetchData = useCallback(async () => {
+  const [error, setError] = useState(null);
+  const fetchData = async () => {
     setLoading(true);
-
-    const dbComments = await firestore
-      .collection("comments")
-      .where("club", "==", clublink)
-      .orderBy("date", "asc")
-      .get();
-    let newComments = [];
-    dbComments.forEach((comment) => {
-      newComments.push(comment.data());
-    });
-    console.log({ newComments });
-    setComments(newComments);
+    try {
+      const dbComments = await firestore
+        .collection("comments")
+        .where("club", "==", clublink)
+        .orderBy("date", "asc")
+        .get();
+      let newComments = [];
+      dbComments.forEach(async (comment) => {
+        // const dbUser = await firestore
+        //   .collection("additional userinfo")
+        //   .doc(comment.data().creatorId)
+        //   .get();
+        // const creatorName = dbUser.data().name;
+        newComments.push({ ...comment.data() /* creatorName*/ });
+      });
+      setComments(newComments);
+    } catch (e) {
+      setError(e);
+    }
     setLoading(false);
-  }, [clublink]);
+  };
   useEffect(() => {
     fetchData();
-  }, [fetchData]);
-  if (loading) return "Loading...";
-  if (!loading && !comments) return "";
+  }, []);
   return (
-    <div>
-      <ul>
-        {comments.map((val, i) => (
-          <li key={i}>{val.content}</li>
-        ))}
-      </ul>
-    </div>
+    <>
+      {error && <div>{error}</div>}
+      {loading
+        ? "Loading"
+        : comments !== [] && (
+            <div>
+              <ul>
+                {comments.map((val, i) => {
+                  console.log({ val });
+                  return <li key={i}>{val.content}</li>;
+                })}
+              </ul>
+            </div>
+          )}
+    </>
   );
 };
 
