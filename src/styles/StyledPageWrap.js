8d319bce3aa27b91b812c79f26efe513d5e4d5e5
dawit jeong <wjdekdnlt1215@gmail.com>
2020-11-17 16:43:10 +0900

    change style of article title

--- src/styles/StyledPageWrap.js
+++ src/styles/StyledPageWrap.js
@@ -4,7 +4,6 @@ const ContentTemplate = styled.div`
   width: 100%;
   /* padding-top: 3%; */
   /* height: 100vh; */
-
   @media only screen and (min-height: 101vh) {
     height: 100%;
     margin: 0 auto;
@@ -14,6 +13,9 @@ const ContentTemplate = styled.div`
 const ContentJustify = styled.div`
   width: 90%;
   margin: 0 auto;
+  background: mintcream;
+  border-radius: 10px;
+  border: 1px solid #d7d9d9;
 `;
 
 export { ContentJustify, ContentTemplate };
