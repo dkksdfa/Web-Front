2a5796b97699139283201580bf112b0e990b6099
dawit jeong <wjdekdnlt1215@gmail.com>
2021-01-02 19:56:15 +0900

    [refactoring] nav, constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -9,6 +9,19 @@ const constants = {
     BUTTON_TEXT: "소개 영상 보러가기",
     VIDEO_URL: "https://youtu.be/M6E-Ri4AGrY",
   },
+
+  NAV: {
+    HOME_TEXT: "Home",
+    SCHOOL_TEXT: "School",
+    SW_TEXT: "SW",
+    FOOD_TEXT: "Food",
+    FINANCE_TEXT: "Finance",
+    OTHER_TEXT: "Other",
+    MODIFY_TEXT: "Modify",
+    JOIN_TEXT: "Join",
+    LOGOUT_TEXT: "Logout",
+    LOGIN_TEXT: "Login",
+  },
 };
 
 export default constants;
--- src/common/nav/index.js
+++ src/common/nav/index.js
@@ -3,57 +3,45 @@ import { ButtonPosition, NavButton, Navigation } from "./StyledNav";
 import { Link, useHistory } from "react-router-dom";
 import { Userinfo } from "../../App";
 import { authService } from "../../firebase";
+import constants from "../constants";
+import NavContainer from "./NavContainer";
 
 const Nav = () => {
   const history = useHistory();
-  const menulist = (login) => [
-    { title: "Home", link: "/" },
-    { title: "School", link: "/School" },
-    { title: "SW", link: "/clubs/SW" },
-    { title: "Food", link: "/clubs/Food" },
-    { title: "Finance", link: "/clubs/Finance" },
-    { title: "Other", link: "/clubs/Other" },
-    login === true
-      ? {
-          title: "Modify",
-          link: "/Modify",
-        }
-      : { title: "Join", link: "/Join" },
-  ];
+  const {
+    HOME_TEXT,
+    SCHOOL_TEXT,
+    SW_TEXT,
+    FOOD_TEXT,
+    FINANCE_TEXT,
+    OTHER_TEXT,
+    MODIFY_TEXT,
+    JOIN_TEXT,
+    LOGOUT_TEXT,
+    LOGIN_TEXT,
+  } = constants.NAV;
 
-  const onClick = () => {
+  const onLogoutButtonClick = () => {
     authService.signOut();
     history.go(0);
   };
-  return (
-    <Userinfo.Consumer>
-      {({ isLoggedIn }) => (
-        <Navigation>
-          <ButtonPosition>
-            {menulist(isLoggedIn).map((val, i) => (
-              <NavButton key={i}>
-                <Link
-                  to={val.link}
-                  style={{
-                    color: "unset",
-                  }}
-                >
-                  {val.title}
-                </Link>
-              </NavButton>
-            ))}
-            {isLoggedIn ? (
-              <NavButton onClick={onClick}>Logout</NavButton>
-            ) : (
-              <NavButton>
-                <Link to="/login">Login</Link>
-              </NavButton>
-            )}
-          </ButtonPosition>
-        </Navigation>
-      )}
-    </Userinfo.Consumer>
-  );
+
+  const itemList = (login) => [
+    { title: HOME_TEXT, link: "/" },
+    { title: SCHOOL_TEXT, link: "/School" },
+    { title: SW_TEXT, link: "/clubs/SW" },
+    { title: FOOD_TEXT, link: "/clubs/Food" },
+    { title: FINANCE_TEXT, link: "/clubs/Finance" },
+    { title: OTHER_TEXT, link: "/clubs/Other" },
+    login === true
+      ? { title: MODIFY_TEXT, link: "/Modify" }
+      : { title: JOIN_TEXT, link: "/Join" },
+    login === true
+      ? { title: LOGOUT_TEXT, link: "", onClick: onLogoutButtonClick }
+      : { title: LOGIN_TEXT, link: "/login" },
+  ];
+
+  return <NavContainer Userinfo={Userinfo} itemList={itemList} />;
 };
 
 export default Nav;
